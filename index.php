610046e56843f0b44065be35d79bbf428a12b240
bennington purcell <bennington.purcell@gmail.com>
2013-03-06 18:48:00 -0500

    more pages

--- index.php
+++ index.php
@@ -1,5 +1,5 @@
 <?php include 'partials/header.php'; ?>
-    <div data-role="page" id="test" data-fullscreen="true">
+    <div data-role="page" id="splash" data-fullscreen="true">
 
         <div data-role="header">
             <img border="0" src="img/header.png" alt="Logo, Facebook" style="display:inline"/> 
@@ -9,13 +9,13 @@
             <h2>Welcome to NSA Mobile</h2>
             <p>A field tool for contractors and architects, this mobile guide is a quick reference to information published in NSA's <strong>Slate Roofs Design and Installation Manual</strong>.</p>    
             <h3>Please select an option::</h3>
-            <a href="#roof_installation" data-role="button">Roof Installation Techniques</a>
+            <a href="#home" data-role="button">Roof Installation Techniques</a>
             <a href="http://nsaslate.com/" data-role="button">Full NSA Website</a>
 
         </div><!-- /content -->
 
     </div>
-    <div data-role="page" id="roof_installation" data-fullscreen="true">
+    <div data-role="page" id="home" data-fullscreen="true">
 
         <div data-role="header">
             <img border="0" src="img/header.png" alt="Logo, Facebook" style="display:inline"/> 
@@ -34,138 +34,9 @@
 
     </div>
 
-<!--
-||
-||  Nails and Nailing
-||
--->
-<div data-role="page" id="page1" data-fullscreen="true">
+<?php include 'partials/nails.php'; ?>
+<?php include 'partials/headlap.php'; ?>
+<?php include 'partials/repair.php'; ?>
 
-    <div data-role="header">
-        <a href="index.html" data-icon="home">Home</a>
-        <h1>Nails and Nailing</h1> 
-    </div><!-- /header -->
-    
-    <div data-role="content">
-        <p>Hundreds of years of experience has shown that the method fastening the slate can make a signifi- cant difference in the reliability and longevity of the roof. We offer several suggestions here.</p>
-<h3>Please select:</h3>
-
-        <ul data-role="listview" data-inset="true">
-            <li><a href="details1.html">Nailing Techinque Details</a></li>
-            <li><a href="#image1">Illustrations and Drawings</a></li>
-        </ul>
-
-    </div><!-- /content -->
-
-</div>
-
-<div data-role="page" id="image1" class="image_wrap" data-fullscreen="true">
-
-    <div data-role="header">
-        <a href="#page1" data-icon="back">Back&nbsp;&nbsp;&nbsp;</a>
-        <h1>Nails and Nailing</h1>
-        <a href="details1.html" data-rel="dialog" data-icon="bars">Details</a>
-    </div><!-- /header -->
-
-    <div data-role="content">    
-        
-        <img class="zoom" src="img/1/1.gif" width="2550" height="3300">
-
-    </div><!-- /content -->
-    
-    <div data-role="footer">
-        <input type="range" name="slider" id="slider" class="slider" value="1" min="1" max="14" data-folder="1"  />
-    </div><!-- /footer -->
-</div>
-
-<!--
-||
-||  Nails and Nailing
-||
--->
-<div data-role="page" id="page2" data-fullscreen="true">
-
-    <div data-role="header">
-        <a href="index.html" data-icon="home">Home</a>
-        <h1>Headlap and Exposure</h1> 
-    </div><!-- /header -->
-    
-    <div data-role="content">
-        <p>Hundreds of years of experience has shown that the method fastening the slate can make a signifi- cant difference in the reliability and longevity of the roof. We offer several suggestions here.</p>
-<h3>Please select:</h3>
-
-        <ul data-role="listview" data-inset="true">
-            <li><a href="details2.html">Headlap and Exposure Details</a></li>
-            <li><a href="#image2">Illustrations and Drawings</a></li>
-        </ul>
-
-    </div><!-- /content -->
-
-</div>
-
-<div data-role="page" id="image2" class="image_wrap" data-fullscreen="true">
-
-    <div data-role="header">
-        <a href="#page1" data-icon="back">back</a>
-        <h1>Headlap and Exposure</h1>
-        <a href="details1.html" data-rel="dialog" data-icon="bars">Details</a>
-    </div><!-- /header -->
-
-    <div data-role="content">    
-        
-        <img class="zoom" src="img/3a/1.gif" width="2475" height="1957">
-
-    </div><!-- /content -->
-    
-    <div data-role="footer">
-        <input type="range" name="slider" id="slider" class="slider" value="1" min="1" max="5" data-folder="3a"  />
-    </div><!-- /footer -->
-
-</div>
-
-<!--
-||
-||  Nails and Nailing
-||
--->
-<div data-role="page" id="page3" data-fullscreen="true">
-
-    <div data-role="header">
-        <a href="index.html" data-icon="home">Home</a>
-        <h1>Offset</h1> 
-    </div><!-- /header -->
-    
-    <div data-role="content">
-        <p>Hundreds of years of experience has shown that the method fastening the slate can make a signifi- cant difference in the reliability and longevity of the roof. We offer several suggestions here.</p>
-<h3>Please select:</h3>
-
-        <ul data-role="listview" data-inset="true">
-            <li><a href="details3.html">Headlap and Exposure Details</a></li>
-            <li><a href="#image3">Illustrations and Drawings</a></li>
-        </ul>
-
-    </div><!-- /content -->
-
-</div>
-
-<div data-role="page" id="image3" class="image_wrap" data-fullscreen="true">
-
-    <div data-role="header">
-        <a href="#page1" data-icon="back">back</a>
-        <h1>Offset</h1>
-        <a href="details3.html" data-rel="dialog" data-icon="bars">Details</a>
-    </div><!-- /header -->
-
-    <div data-role="content">    
-        
-        <img class="zoom" src="img/3b/1.gif" width="2475" height="1957">
-
-    </div><!-- /content -->
-    
-    <div data-role="footer">
-        <input type="range" name="slider" id="slider" class="slider" value="1" min="1" max="5" data-folder="3b"  />
-    </div><!-- /footer -->
-
-</div>
 </body>
 </html>
---
+++ partials/nails.php
@@ -0,0 +1,58 @@
+<div data-role="page" id="nails" data-fullscreen="true">
+
+    <div data-role="header">
+        <a href="index.html" data-icon="home">Home</a>
+        <h1>Nails and Nailing</h1> 
+    </div><!-- /header -->
+    
+    <div data-role="content">
+        <p>Hundreds of years of experience has shown that the method fastening the slate can make a signifi- cant difference in the reliability and longevity of the roof. We offer several suggestions here.</p>
+<h3>Please select:</h3>
+
+        <ul data-role="listview" data-inset="true">
+            <li><a href="#nails_detail">Details</a></li>
+            <li><a href="#nails_image">Drawings</a></li>
+        </ul>
+
+    </div><!-- /content -->
+
+</div>
+
+<div data-role="page" id="nails_image" class="image_wrap" data-fullscreen="true">
+    <div data-role="header">
+        <h1>Headlap and Exposure</h1>
+    </div><!-- /header -->
+    <div data-role="content">
+        <img class="zoom" src="img/nails/1.gif" width="2475" height="1957">
+    </div><!-- /content -->
+    <div data-role="footer">
+        <input type="range" name="slider" id="slider" class="slider" value="1" min="1" max="5" data-folder="nails"  />
+        <div data-role="navbar"  data-iconpos="left">
+              <ul>
+                  <li><a href="" data-icon="back" data-rel="back">Back</a></li>
+                  <li><a href="#home" data-icon="home">Home</a></li>
+                  <li><a href="headlap_details.html" data-rel="dialog" data-icon="bars">Details</a></li>
+              </ul>
+          </div><!-- /navbar -->
+    </div><!-- /footer -->
+</div>
+
+<div data-role="page" id="nails_detail">
+    <div data-role="header">
+        <a href="#nails" data-icon="back">Back</a>
+        <h1>Nails and Nailing</h1>
+    </div><!-- /header -->
+    <div data-role="content">
+        <h3>Basic Practice</h3>
+        <p>Nailing: Set the nail in the countersink in the top surface of the slate.  Do not over-nail or under-nail.  Slate shingles must hang on their nails.</p>
+        <p>Nails: 11-gauge, smooth shank, copper wire slating nails are used most frequently.</p>
+        <p>Number of Nails: Use 2 nails per slate.</p>
+        <p>Nail Length:  Generally twice the thickness of the slate plus 1”.</p>
+
+        <h3>Special Considerations</h3>
+        <p>Gypsum and Concrete Roof Decks:  Use smooth shank nails, not ring shank.  Stainless steel nails may be needed to avoid bending.</p>
+        <p>Gauge: Use 10-gauge nails for larger slates, hardwood roof decks, and where longer nails are required (e.g., hips).</p>
+        <p>4 Nails: Use 4 nails per slate for slates measuring ≥ 3/4” in thickness and ≥ 20” in length.  Place the 2 additional nails 2” above the regular nail holes.</p>
+        <p>Flashings: Do not nail slates through flashings.  Move the slating nails up, place one nail above the other on one side of the slate, or use copper wires secured to the roof deck upslope of the flashing.</p>
+    </div><!-- /content -->
+</div>
