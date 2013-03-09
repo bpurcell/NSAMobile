f653363ee2cc6a19aab32fa8ece10ab3ac9adff4
bennington purcell <bennington.purcell@gmail.com>
2013-03-09 17:46:05 -0500

    reorg, new intro text, first image load

--- js/mobileapp.js
+++ js/mobileapp.js
@@ -1,6 +1,6 @@
     $(document).live('pageinit',function(event){
         
-        var h = $(window).height()-130;
+        var h = $(window).height()-64;
         var w = $(window).width();
         $('.image-wrapper').height(h);
 
@@ -8,12 +8,16 @@
             $('.image_wrap .zoom').smoothZoom({
                 width: w,
                 height: h,
-                responsive: false
+                responsive: false,
+                zoom_BUTTONS_SHOW : 'NO',
+                pan_BUTTONS_SHOW : 'NO',
+                pan_LIMIT_BOUNDARY : 'NO'
 
             });
         });
         
         $(".image_wrap .slider").change(function() {
+            $('.information').hide();
             sVal = $(this).val();
             folder = $(this).data('folder');
             
--- partials/header.php
+++ partials/header.php
@@ -3,18 +3,26 @@
 
 <head>
     <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no"> 
+    <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no">
+    <meta name="apple-mobile-web-app-capable" content="yes" />
     <title>NSA</title> 
     <link rel="stylesheet" href="http://code.jquery.com/mobile/1.3.0/jquery.mobile-1.3.0.min.css" />
 
-    <link rel="stylesheet" href="nsa1.3.css" />
-    <link rel="stylesheet" href="additional.css" />
+    <link rel="stylesheet" href="nsa1.3.min.css" />
+    <link rel="stylesheet" href="css/additional.css" />
 
     <script src="http://code.jquery.com/jquery-1.8.2.min.js"></script>
-    <script src="Source/jquery.smoothZoom.js"></script>
+    
+    <script src="js/jquery.smoothZoom.min.js"></script>
 
     <script src="http://code.jquery.com/mobile/1.3.0/jquery.mobile-1.3.0.min.js"></script>
     
     <script src="js/mobileapp.js"></script>
+    
+    <link rel="apple-touch-icon" href="images/apple-touch-icon-iphone.png" /> 
+    <link rel="apple-touch-icon" sizes="72x72" href="images/apple-touch-icon-ipad.png" /> 
+    <link rel="apple-touch-icon" sizes="114x114" href="images/apple-touch-icon-iphone4.png" />
+    <link rel="apple-touch-icon" sizes="144x144" href="images/apple-touch-icon-ipad3.png" />
+    <link rel="shortcut icon" href="favicon.ico"/>
 </head> 
 <body> 
